5cf1ff0c97740c630053b9912e5c7ce62e54ddc2
BanerjeeProdipta <prodiptaj@live.com>
2020-09-25 20:50:00 +0600

    Question

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,12 +6,10 @@
     </template>
 
     <hr class="my-4">
-
-    <p>
-     Answers
-    </p>
-
-    <b-button variant="primary" href="#">Submit</b-button>
+    <b-list-group class="mb-4">
+        <b-list-group-item v-for="(answer, index) in answers" :key="index">{{ answer}}</b-list-group-item>
+    </b-list-group>
+    <b-button class="mr-2" variant="primary" href="#">Submit</b-button>
     <b-button @click="next" variant="success" href="#">Next</b-button>
   </b-jumbotron>
 </div>
@@ -20,8 +18,20 @@
 export default {
     props: {
         currentQuestion: Object,
-        next: Function
-        
+        next: Function 
     },
+    computed:{
+        answers(){
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    mounted(){
+
+    }
 }
 </script>
+
+<style scoped>
+</style>
